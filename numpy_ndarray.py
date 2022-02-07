c4a834a52eef74d5def4c6ac350e1dfc8ab63e8b
shane <qaz56115624@gmail.com>
2022-02-07 11:27:23 +0800

    ok

---
+++ numpy_ndarray.py
@@ -0,0 +1,49 @@
+import numpy as np
+# 建立一維陣列
+# data = np.array([3,2,6,4])
+# print(data)
+# data = np.empty(4)
+# print(data)
+# data = np.zeros(3)
+# print(data)
+# data = np.ones(3)
+# print(data)
+# data = np.arange(5)
+# print(data)
+# 建立一個二維陣列
+# data = np.array([
+#     [2,3,2],
+#     [1,5,2],
+#     [4,2,1,]
+# ])
+# print(data)
+# data = np.empty([3,3]) # 創造一個3*3的陣列,資料未指定
+# print(data)
+# data = np.ones([2,3])
+# print(data)
+# 建立一個三維陣列
+# data = np.array([ # 創造一個2*2*2的三維陣列
+#     [
+#         [3,1],[1,2]
+#     ],
+#     [
+#         [2,5],[10,2]
+#     ]
+# ])
+# print(data)
+# data = np.zeros([3,1,3])
+# print(data)
+
+# 建立高維陣列
+# data = np.array([ # 創造一個1x1x2x3的四維陣列
+#     [
+#         [
+#             [3,2,1],
+#             [5,5,10]
+#         ]
+#     ]
+# ]
+# )
+# print(data)
+data = np.ones([2,1,1,2]) # 創造一個2x1x1x2的四維陣列,資料都是1
+print(data)
---
+++ numpy_ndarray_op.py
@@ -0,0 +1,15 @@
+import numpy as np
+# 逐元運算(elementwise)
+data1 = np.array([3,2,10])
+data2 = np.array([1,3,6])
+result = data1+data2
+print("加法",result)
+result = data1*data2
+print("乘法",result)
+result = data1>data2
+print("大於",result)
+result = data1==data2
+print("是否相等",result)
+# 矩陣運算(matrix)
+
+# 統計運算(statics)
